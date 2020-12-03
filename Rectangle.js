74e8722d6f06add3befec0079c1d2da854929164
hoang <phanhuyhoanghp@gmail.com>
2020-12-03 13:53:24 +0700

    bt

--- Rectangle.js
+++ Rectangle.js
@@ -13,20 +13,50 @@
 // this.draw = function () {
 //     document.write('Hinh chữ nhật có chiều dài: ' + this.width + ' chiều rộng' + this.height);
 // }
-function Rectangle(width,height) {
-    this.width=width;
-    this.height=height;
+// function Rectangle(width,height) {
+//     this.width=width;
+//     this.height=height;
+//     this.area=function () {
+//         return this.width*this.height;
+//         // return width*height;
+//
+//     }
+//     this.perimeter=function () {
+//         return (this.width+this.height)*2;
+//     }
+//     this.draw=function () {
+//         document.write("<br>Hinh chu nhat co chieu dai: "+this.width+" chieu rong: "+this.height);
+//     }
+//
+// }
+class Rectangle{
+    constructor (width,height){
+        this.width=width;
+        this.height=height;
+    }
 
-    this.area=function () {
+    get width() {
+        return  this.width;
+    }
+
+    set width(value) {
+        this.width = value;
+    }
+
+    get height() {
+        return this.height;
+    }
+
+    set height(value) {
+        this.height = value;
+    }
+    get area() {
         return this.width*this.height;
-        // return width*height;
-
     }
-    this.perimeter=function () {
-        return (this.width+this.height)*2;
-    }
-    this.draw=function () {
-        document.write("<br>Hinh chu nhat co chieu dai: "+this.width+" chieu rong: "+this.height);
+    get perimeter() {
+        return (this.height+this.width)*2;
     }
 
 }
+
+
