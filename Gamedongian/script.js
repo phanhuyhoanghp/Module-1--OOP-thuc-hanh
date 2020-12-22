5fa406075204e1d9415e11e8f0ded17697e2673f
hoang <phanhuyhoanghp@gmail.com>
2020-12-22 10:54:00 +0700

    bt

---
+++ Gamedongian/script.js
@@ -0,0 +1,31 @@
+function Hero(image, top, left, size){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    }
+
+    this.moveRight = function(){
+        this.left += 0.25;
+        console.log('ok: ' + this.left);
+    }
+
+}
+
+var hero = new Hero('pikachu.png', 50, 100, 200);
+
+function start(){
+    if(hero.left < window.innerWidth - hero.size){
+        hero.moveRight();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 0.5)
+}
+
+start();
